efb671b7ae3d2eae1129492701386e1c192505b4
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-28 18:21:08 -0400

    updated files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,9 @@
 const FILES_TO_CACHE = [
-  '/index.html'
+  '/index.html',
+  '/index.js',
+  'manifest.webmanifest',
+  '/icons/icon-192x192.png',
+  'icon-512x512.png'
 ];
 
 const CACHE_NAME = "static-cache-v2";
