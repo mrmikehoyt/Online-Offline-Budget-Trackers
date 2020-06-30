13778507a326192bbbff2541175ebc58f4b74126
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-29 21:37:40 -0400

    modified comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,12 @@
 const FILES_TO_CACHE = [
+  '/',
   '/index.html',
   '/index.js',
   'manifest.webmanifest',
   '/icons/icon-192x192.png',
-  'icon-512x512.png'
+  '/icons/icon-512x512.png',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -11,18 +14,23 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 // install serviceworker
 self.addEventListener("install", function(evt) {
-  console.log('service worker installed')
- 
+ // console.log('service worker installed')
+  evt.waitUntil(
+  caches.open(CACHE_NAME).then(cache => {
+    console.log('caching files')
+cache.addAll(FILES_TO_CACHE)
+  }))
     })
   
 //activate service worker
 self.addEventListener('activate', evt =>{
-  console.log ('service worker activated')
+  //console.log ('service worker activated')
 })
 
 //created fetch event when getting stuff from server
 self.addEventListener('fetch', evt =>{
   console.log('fetch event',evt);
+  
 })
 
 //self.skipWaiting();
