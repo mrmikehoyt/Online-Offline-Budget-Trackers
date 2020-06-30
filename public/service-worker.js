2e22254fa33d824bca9f1a89bf2257d03e029d8c
Michael Hoyt <m.mikehoyt@outlook.com>
2020-06-29 21:47:19 -0400

    updated comments and commented out datacache temporarily

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,7 @@ const FILES_TO_CACHE = [
 ];
 
 const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+//const DATA_CACHE_NAME = "data-cache-v1"; -commented out 9:46 do not believe needed at this time
 
 // install serviceworker
 self.addEventListener("install", function(evt) {
