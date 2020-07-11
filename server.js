6f2fe76ae3ee86c8c2d49504d81a51ab085a6e99
Michael Hoyt <m.mikehoyt@outlook.com>
2020-07-11 00:28:14 -0400

    Forgot require mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const mongodb = require('mongodb');
 
 
 
